de9a28ffdde317c7ae9a4a6864cf85873699b66b
Frederic TRAN <frederictran@hotmail.fr>
2022-05-10 15:46:38 +0000

    Add test unit

---
+++ src/test/java/fr/frederic94500/cryptoprinter/TestCrypto.java
@@ -0,0 +1,15 @@
+package fr.frederic94500.cryptoprinter;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestCrypto {
+    @Test
+    public void testCrypto() throws IOException, InterruptedException {
+        var crypto = new Crypto("https://api.binance.com/api/v3/ticker/24hr?symbol=", "BTCUSDT");
+        assertTrue(crypto.isCorrect());
+    }
+}
